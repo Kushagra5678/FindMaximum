0753d162a4d1ab75755eec77c17c31b35ac5f767
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-06 21:51:31 +0530

    modify: Added print Console function

--- src/main/java/com/practice/FindMaximum/FindMaxMain.java
+++ src/main/java/com/practice/FindMaximum/FindMaxMain.java
@@ -14,12 +14,17 @@ public class FindMaxMain<E extends Comparable<E>> {
 		Optional<E> check = Optional.ofNullable(mf.array[mf.array.length - 1]);
 		if (check != null) {
 			Arrays.sort(mf.array, Collections.reverseOrder());
+			printMaximum(mf.array[0]);
 			return (E) mf.array[0];
 		} 
 		else {
 			System.out.println("Array is empty!!!!");
 			return null;
 		}
+	}
+	
+	public static <E extends Comparable> void  printMaximum(E maximum){
+		System.out.println("Maximum value is "+ maximum);
 	}
 
 }
