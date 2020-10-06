9d16515d45e6ceb87e1879af16b5dbfaec99c1d6
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-06 19:32:52 +0530

    modify: methods modified to generic

--- src/main/java/com/practice/FindMaximum/FindMaxMain.java
+++ src/main/java/com/practice/FindMaximum/FindMaxMain.java
@@ -1,30 +1,12 @@
 package com.practice.FindMaximum;
 
 public class FindMaxMain {
-	public String findMaxNumber(String a, String b, String c) {
-		String max=a;
-		if(b.compareTo(max)>0) 
-			max=b;
-		if(c.compareTo(max)>0) 
-			max=c;
-		return max;
-	}
-	
-	public Float findMaxNumber(Float a, Float b, Float c) {
-		Float max=a;
+	public <E extends Comparable<E>>  E findMaxNumber(E a,E b,E c) {
+		E max=a;
 		if(b.compareTo(max)>0)
 			max=b;
-		if(c.compareTo(max)>0) 
+		if(c.compareTo(max)>0)
 			max=c;
-		return max;
-	}
-	
-	public Integer findMaxNumber(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
 		return max;
 	}
 
